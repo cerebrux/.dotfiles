f514241a124b93b7a57e0814d532bb1b2953f8a4
Salih Emin <cerebrux@users.noreply.github.com>
2020-02-19 18:17:54 +0200

    restructure

--- .clone-dotfiles.sh
+++ .clone-dotfiles.sh
@@ -1,10 +1,17 @@
-echo "Downloading .dotfiles"
-git clone --bare https://github.com/cerebrux/.dotfiles.git $HOME/.dotfiles
+#!/usr/bin/env sh
+set -e
+
 function dotfile {
    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
+
+clear
+echo "Downloading .dotfiles"
+sleep 2
+git clone --bare https://github.com/cerebrux/.dotfiles.git $HOME/.dotfiles
 echo ""
 echo ""
+sleep 2
 echo "Checking if backup is neccesary for your .dotfiles"
 mkdir -p .config-backup
 dotfile checkout
@@ -15,10 +22,11 @@ if [ $? = 0 ]; then
     dotfile checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
     echo "Your previous .dotfiles are backed up at .config-backup folder"
 fi;
+sleep 1
 dotfile checkout
 dotfile config status.showUntrackedFiles no
 echo ""
 echo "Downloading Vim-Plug for NeoVim and Updating Plugins"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +PlugUpdate +qall
+nvim +PlugInstall +qall
 echo "You are now ready !"
